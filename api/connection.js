1b68d4791a61a94cad68bd8ed85901e21aec570f
jelyanjake <jelyanjake@gmail.com>
2025-05-18 16:45:36 +0800

    f

--- api/connection.js
+++ api/connection.js
@@ -3,7 +3,7 @@ import clientPromise from "../lib/mongo.js";
 export default async function handler(req, res) {
   try {
     const client = await clientPromise;
-    const db = client.db("kanna");
+    const db = client.db("logbox");
     const users = await db.collection("users").find({}).toArray();
     res.status(200).json(users);
   } catch (e) {
